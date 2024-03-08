c83fbb205b69444f2e1e3d6c02ee69cde47d1590
Nostra <nostra@192.168.1.5>
2024-03-08 13:25:51 +0700

    initialex empty project

---
+++ Toplists/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Toplists
+//
+//  Created by Nostra on 3/8/24.
+//  Copyright © 2024 Nostratech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+}
+
